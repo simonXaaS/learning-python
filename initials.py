c1f1093fbbf0ff175c793a76d2dc28b00f6636d8
Simon Blahut <simonblahut.sb@gmail.com>
2023-02-11 10:15:21 +0000

    commiting initials project

---
+++ initials.py
@@ -0,0 +1,10 @@
+# Simon Blahut
+# Printing out my initials
+
+print(" SSS  BBBB ")
+print("S   S B   B")
+print("S     B   B")
+print(" SSS  BBBB")
+print("    S B   B")
+print("S   S B   B")
+print(" SSS  BBBB")
